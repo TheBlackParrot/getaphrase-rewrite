d3868125935b342a184a380e9a8441c89101e6c4
TheBlackParrot <theblackparrot0@gmail.com>
2016-08-24 01:51:40 -0500

    we're good for now i think?

--- web/index.php
+++ web/index.php
@@ -26,6 +26,13 @@
 		$data = json_decode($out, true);
 		$data_j = $out;
 	} else {
+		if(!$data['phrase']) {
+			$root = dirname(dirname(__FILE__));
+			$out = str_replace('\'', '\\\'', shell_exec('cd ' . $root . '; ./main.py 2>&1'));
+
+			$data = json_decode($out, true);
+			$data_j = $out;
+		}
 		$data_j = str_replace('\'', '\\\'', json_encode($data));
 	}
 
@@ -37,6 +44,37 @@
 		$bg = $colors["light"][mt_rand(0, count($colors["light"])-1)];
 		$fg = $colors["dark"][mt_rand(0, count($colors["dark"])-1)];
 	}
+
+	$effects = [];
+	switch(mt_rand(0, 5)) {
+		case 1: // dark shadow
+			$effects[] = "text-shadow: 0px 2px 4px rgba(0, 0, 0, 0.75);";
+			break;
+
+		case 2: // light shadow
+			$effects[] = "text-shadow: 0px 1px 0px rgba(255, 255, 255, 0.75);";
+			break;
+
+		case 3: // outline
+			$amount = mt_rand(1, 3);
+			$effects[] = "text-shadow: -{$amount}px -{$amount}px 0 #000, {$amount}px -{$amount}px 0 #000, -{$amount}px {$amount}px 0 #000, {$amount}px {$amount}px 0 #000;";
+			break;
+
+		case 4: // emboss
+			$effects[] = "text-shadow: -1px -1px 0px rgba(255,255,255,0.4), 1px 1px 0px rgba(0,0,0,0.8);";
+			break;
+
+		case 5: // retro
+			$spacing = mt_rand(2, 4);
+			$direction = mt_rand(-1, 1);
+			$offset = $spacing*$direction;
+			$offset_double = $offset*2;
+			$spacing_double = $spacing*2;
+			$css[] = "text-shadow: {$offset}px {$spacing}px 0px #{$bg}, {$offset_double}px {$spacing_double}px 0px rgba(0,0,0,0.5);";
+			break;
+	}
+
+	$weight = mt_rand(4, 7)*100;
 ?>
 
 <html>
@@ -48,7 +86,8 @@
 
 	<script type="text/javascript" src="js/jquery.js"></script>
 	<script>
-		var data = JSON.parse('<?php echo trim($data_j); ?>');
+		var raw_data = '<?php echo trim($data_j); ?>';
+		var data = JSON.parse(raw_data);
 	</script>
 	<style>
 		@import 'https://fonts.googleapis.com/css?family=<?php echo $font_safe; ?>';
@@ -59,6 +98,11 @@
 		#phrase {
 			font-family: "<?php echo $font; ?>", sans-serif;
 			font-size: 10vw;
+			font-weight: <?php echo $weight; ?>;
+			<?php foreach($effects as $effect) {
+				echo($effect);
+			}
+			?>
 		}
 	</style>
 </head>
@@ -68,13 +112,14 @@
 		<div id="phrase"><?php echo $data['phrase']; ?></div>
 		<?php
 			if(!$data['phrase']) {
-				echo "<span>something has gone wrong here, just refresh.</span>";
+				echo '<script>console.log("$data[\"phrase\"] was empty, fell back to JS to render text");$("#phrase").text(data.phrase);</script>';
 			}
 		?>
 	</div>
 	<div class="bottom">
 		<div class="left">
 			<a href="https://twitter.com/getaphrase" class="twitter-follow-button" data-show-count="true">Follow @getaphrase</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+			<iframe style="margin-left: 24px;" src="https://ghbtns.com/github-btn.html?user=theblackparrot&repo=getaphrase-2&type=star&count=true" frameborder="0" scrolling="0" width="170px" height="20px"></iframe>
 		</div>
 		<div class="right">
 			<a href="index.php?id=<?php echo $data['id']; ?>">permalink</a>

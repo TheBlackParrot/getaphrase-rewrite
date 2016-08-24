5f90a406d74f8123dc9376dbc3a68632942ab551
TheBlackParrot <theblackparrot0@gmail.com>
2016-08-23 20:10:13 -0500

    verbs!

--- generator.py
+++ generator.py
@@ -2,11 +2,14 @@ import json;
 import sys;
 import random;
 import re;
+import copy;
 
 vowels = ["a", "e", "i", "o", "u"];
 nouns = [];
 adjectives = [];
 phrases = [];
+verbs = [];
+adverbs = [];
 
 try:
 	with open('nouns.json', 'r') as file:
@@ -25,6 +28,18 @@ try:
 		phrases = json.load(file);
 except:
 	sys.exit("couldn't load phrases!");
+
+try:
+	with open('verbs.json', 'r') as file:
+		verbs = json.load(file);
+except:
+	sys.exit("couldn't load verbs!");
+
+try:
+	with open('adverbs.json', 'r') as file:
+		adverbs = json.load(file);
+except:
+	sys.exit("couldn't load adverbs!");
 
 
 proper_nouns = [];
@@ -132,6 +147,45 @@ class Adjective():
 	def __str__(self):
 		return " ".join(str(part) for part in self.parts);
 
+
+class Verb():
+	def __init__(self, args, word=None, past_tense=None, ing=None):
+		global verbs;
+
+		self.args = args;
+
+
+		self.word = word;
+		self.past_tense = past_tense;
+		self.ing = ing;
+
+		self.final = self.word;
+
+	def to_ing(self):
+		self.final = self.ing;
+
+	def to_future_tense(self):
+		self.final = " ".join(["will", self.word]);
+
+	def to_past_tense(self):
+		self.final = self.past_tense;
+
+	def __str__(self):
+		return self.final;
+
+
+class Adverb():
+	def __init__(self, args):
+		global adverbs;
+
+		self.args = args;
+
+		self.word = random.choice(adverbs);
+
+	def __str__(self):
+		return self.word;
+
+
 class AfterStr():
 	def __init__(self, data):
 		self.data = data;
@@ -154,6 +208,7 @@ class TemplateString():
 		global nouns;
 		global proper_nouns;
 		global nonproper_nouns;
+		global verbs;
 		global vowels;
 		global reg_safe_part;
 
@@ -168,6 +223,22 @@ class TemplateString():
 				if after:
 					args.append(AfterStr(after));
 
+				if "again" in args:
+					multiple = False;
+					print(self.parts);
+					print(list(reversed(self.parts)));
+					if args[0].title() in globals():
+						for part in list(reversed(self.parts)):
+							if type(part) == globals()[args[0].title()]:
+								self.parts.append(copy.deepcopy(part));
+								if after:
+									self.parts[len(self.parts)-1].args.append(AfterStr(after));
+								multiple = True;
+								break;
+
+					if multiple:
+						continue;
+
 				if args[0] == "noun":
 					if "proper" in args:
 						noun = random.choice(proper_nouns);
@@ -176,13 +247,21 @@ class TemplateString():
 					else:
 						noun = random.choice(nouns);
 
-					self.parts.append(Noun(args, **noun));
+					if noun:
+						self.parts.append(Noun(args, **noun));
 
 				elif args[0] == "article":
 					self.parts.append(Article(args));
 
 				elif args[0] == "adjective":
 					self.parts.append(str(Adjective(args)));
+
+				elif args[0] == "verb":
+					self.parts.append(Verb(args, **random.choice(verbs)));
+
+				elif args[0] == "adverb":
+					self.parts.append(Adverb(args));
+
 			else:
 				self.parts.append(raw_part);
 
@@ -212,6 +291,14 @@ class TemplateString():
 							_ = part.args
 							part = Noun(_, **random.choice(nouns));
 
+			if type(part) == Verb:
+				if "ing" in part.args:
+					part.to_ing();
+				elif "past" in part.args:
+					part.to_past_tense();
+				elif "future" in part.args:
+					part.to_future_tense();
+
 
 		if self.plural_phrase:
 			self.phrase_is_plural();
@@ -239,12 +326,10 @@ class TemplateString():
 for i in range(0, 20):
 	main_choices = [
 		"[adjective] [noun]",
-		"[article] [adjective] [noun]",
-		"omg, i love [adjective] [noun:plural]!",
-		"hey [noun:proper], here's your [adjective] [noun]"
+		"[article] [adjective] [noun]"
 	]
 	template = random.choice(main_choices);
-	if not random.randint(0, 3):
+	if not random.randint(0, 2):
 		template = random.choice(phrases);
 
 	test = TemplateString(template);
